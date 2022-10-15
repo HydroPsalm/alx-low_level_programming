d15fb3cbb19dd598c21997bc4783bbd4ec31e046
Samuel <geypsalm.01@gmail.com>
2022-10-14 17:59:59 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main - print lowercase and uppercase a-zA-Z using putchar
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+char lower_apha = 'a';
+char upper_alpha = 'A';
+while (lower_alpha <= 'z') /*print lowercases a-z*/
+{
+putchar(lower_alpha);
+lower_alpha++;
+}
+while (upper_alpha <= 'Z') /*print uppercase A-Z*/
+{
+putchar(upper_alpha);
+upper_alpha++;
+}
+putchar('\n');
+return (0); }
