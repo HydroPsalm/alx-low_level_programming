8e3a7136e3100a01c7224dec4a1978bdae4a15cb
Samuel <geypsalm.01@gmail.com>
2022-10-19 17:02:36 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,17 +5,17 @@
  */
 void print_alphabet_x10(void)
 {
-int times = 0
-char alphabet = 'a'
+int times = 0;
+char alphabet = 'a';
 while (times < 10)
 {
-alphabet = 'a'
+alphabet = 'a';
 while (alphabet <= z)
 {
 _putchar (alphabet);
 alphabet++;
 }
-_putchar ('\n')
+_putchar ('\n');
 }
 times++;
 }
