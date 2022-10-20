9f53888d1a0b9162c8f55fe4825c0dc845afe5a2
Samuel <geypsalm.01@gmail.com>
2022-10-19 18:41:17 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *_abs - prints absolute value of a digit
+ *@n: n is an integer
+ *Return: integer
+ */
+int _abs(int n)
+{
+if (n > 0)
+{
+return (n);
+}
+else if (n < 0)
+{
+return (-n);
+}
+else
+{
+return (n);
+}
+}
